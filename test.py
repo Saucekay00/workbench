872207b96bd87a0d9fbffd00e1f35d5f254db23a
Saucekay00 <scakravrthi04@gmail.com>
2025-02-20 09:35:29 +0800

    test.py

--- test.py
+++ test.py
@@ -600,5 +600,5 @@ def index():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=8000)
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(host='0.0.0.0', port=8001)
+    serve(app, host="0.0.0.0", port=8001)
